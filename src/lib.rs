b61ed6cd909e14c26442ce8d1ccc3b6f909f62ee
Antoine Gersant <antoine.gersant@lesforges.org>
2019-01-05 18:58:21 -0800

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@
 extern crate failure;
 
 use failure::Error;
+use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::fs::File;
 use std::path::Path;
 use std::time::Duration;
 
@@ -45,47 +45,77 @@ enum Mode {
     Mono,
 }
 
-static BIT_RATES: [[[u32; 16]; 4]; 3] = [[
-        [0;16],
-        [0, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 0], // Mpeg1 Layer1
-        [0, 32, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384, 0],    // Mpeg1 Layer2
-        [0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0],     // Mpeg1 Layer3
+static BIT_RATES: [[[u32; 16]; 4]; 3] = [
+    [
+        [0; 16],
+        [
+            // Mpeg1 Layer1
+            0, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 0,
+        ],
+        [
+            // Mpeg1 Layer2
+            0, 32, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384, 0,
+        ],
+        [
+            // Mpeg1 Layer3
+            0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0,
+        ],
     ],
-                                         [
-        [0;16],
-        [0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256, 0],   // Mpeg2 Layer1
-        [0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0],        // Mpeg2 Layer2
-        [0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0],        // Mpeg2 Layer3
+    [
+        [0; 16],
+        [
+            // Mpeg2 Layer1
+            0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256, 0,
+        ],
+        [
+            // Mpeg2 Layer2
+            0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0,
+        ],
+        [
+            // Mpeg2 Layer3
+            0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0,
+        ],
     ],
-                                         [
-        [0;16],
-        [0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256, 0],   // Mpeg25 Layer1
-        [0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0],        // Mpeg25 Layer2
-        [0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0],        // Mpeg25 Layer3
-    ]];
-
+    [
+        [0; 16],
+        [
+            // Mpeg25 Layer1
+            0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256, 0,
+        ],
+        [
+            // Mpeg25 Layer2
+            0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0,
+        ],
+        [
+            // Mpeg25 Layer3
+            0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0,
+        ],
+    ],
+];
 
 static SAMPLING_RATES: [[u32; 4]; 3] = [
-    [44100, 48000, 32000, 0],   // Mpeg1
-    [22050, 24000, 16000, 0],   // Mpeg2
-    [11025, 12000, 8000, 0],    // Mpeg25
+    [44100, 48000, 32000, 0], // Mpeg1
+    [22050, 24000, 16000, 0], // Mpeg2
+    [11025, 12000, 8000, 0],  // Mpeg25
 ];
 
 static SAMPLES_PER_FRAME: [[u32; 4]; 3] = [
-    [0, 384, 1152, 1152],   // Mpeg1
-    [0, 384, 1152, 576],    // Mpeg2
-    [0, 384, 1152, 576],    // Mpeg25
+    [0, 384, 1152, 1152], // Mpeg1
+    [0, 384, 1152, 576],  // Mpeg2
+    [0, 384, 1152, 576],  // Mpeg25
 ];
 
 static SIDE_INFORMATION_SIZES: [[u32; 4]; 3] = [
-    [32, 32, 32, 17],   // Mpeg1
-    [17, 17, 17, 9],    // Mpeg2
-    [17, 17, 17, 9],    // Mpeg25
+    [32, 32, 32, 17], // Mpeg1
+    [17, 17, 17, 9],  // Mpeg2
+    [17, 17, 17, 9],  // Mpeg25
 ];
 
 fn get_bitrate(version: Version, layer: Layer, encoded_bitrate: u8) -> Result<u32, Error> {
     if encoded_bitrate <= 0 || encoded_bitrate >= 15 {
-        bail!(MP3DurationError::InvalidBitrate { bitrate: encoded_bitrate });
+        bail!(MP3DurationError::InvalidBitrate {
+            bitrate: encoded_bitrate
+        });
     }
     if layer == Layer::NotDefined {
         bail!(MP3DurationError::ForbiddenLayer);
@@ -95,7 +125,9 @@ fn get_bitrate(version: Version, layer: Layer, encoded_bitrate: u8) -> Result<u3
 
 fn get_sampling_rate(version: Version, encoded_sampling_rate: u8) -> Result<u32, Error> {
     if encoded_sampling_rate >= 3 {
-        bail!(MP3DurationError::InvalidSamplingRate { sampling_rate: encoded_sampling_rate });
+        bail!(MP3DurationError::InvalidSamplingRate {
+            sampling_rate: encoded_sampling_rate
+        });
     }
     Ok(SAMPLING_RATES[version as usize][encoded_sampling_rate as usize])
 }
@@ -112,12 +144,12 @@ fn get_side_information_size(version: Version, mode: Mode) -> Result<u32, Error>
 }
 
 fn skip<T>(reader: &mut T, dump: &mut Vec<u8>, advance: usize) -> Result<(), std::io::Error>
-    where T: Read
+where
+    T: Read,
 {
     dump.resize(advance, 0);
     reader.read_exact(&mut dump[..])
 }
-
 
 /// Measures the duration of a mp3 file contained in any struct implementing Read.
 ///
@@ -136,7 +168,8 @@ fn skip<T>(reader: &mut T, dump: &mut Vec<u8>, advance: usize) -> Result<(), std
 /// println!("File duration: {:?}", duration);
 /// ```
 pub fn from_read<T>(reader: &mut T) -> Result<Duration, Error>
-    where T: Read
+where
+    T: Read,
 {
     let mut header_buffer = [0; 4];
     let mut dump = vec![0; 16 * 1024];
@@ -150,11 +183,12 @@ pub fn from_read<T>(reader: &mut T) -> Result<Duration, Error>
         };
 
         // MPEG frame
-        let header = (header_buffer[0] as u32) << 24 | (header_buffer[1] as u32) << 16 |
-                     (header_buffer[2] as u32) << 8 | header_buffer[3] as u32;
+        let header = (header_buffer[0] as u32) << 24
+            | (header_buffer[1] as u32) << 16
+            | (header_buffer[2] as u32) << 8
+            | header_buffer[3] as u32;
         let is_mp3 = header >> 21 == 0x7FF;
         if is_mp3 {
-
             let version = match (header >> 19) & 0b11 {
                 0 => Version::Mpeg25,
                 1 => bail!(MP3DurationError::ForbiddenVersion),
@@ -199,18 +233,21 @@ pub fn from_read<T>(reader: &mut T) -> Result<Duration, Error>
                 Err(ref e) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
                 Err(e) => bail!(e),
             };
-            let is_xing = xing_buffer[0] == 'X' as u8 && xing_buffer[1] == 'i' as u8 &&
-                          xing_buffer[2] == 'n' as u8 &&
-                          xing_buffer[3] == 'g' as u8;
-            let is_info = xing_buffer[0] == 'I' as u8 && xing_buffer[1] == 'n' as u8 &&
-                          xing_buffer[2] == 'f' as u8 &&
-                          xing_buffer[3] == 'o' as u8;
+            let is_xing = xing_buffer[0] == 'X' as u8
+                && xing_buffer[1] == 'i' as u8
+                && xing_buffer[2] == 'n' as u8
+                && xing_buffer[3] == 'g' as u8;
+            let is_info = xing_buffer[0] == 'I' as u8
+                && xing_buffer[1] == 'n' as u8
+                && xing_buffer[2] == 'f' as u8
+                && xing_buffer[3] == 'o' as u8;
             if is_xing || is_info {
                 let has_frames = 0 != (xing_buffer[7] & 1);
                 if has_frames {
-                    let num_frames = (xing_buffer[8] as u32) << 24 | (xing_buffer[9] as u32) << 16 |
-                                     (xing_buffer[10] as u32) << 8 |
-                                     xing_buffer[11] as u32;
+                    let num_frames = (xing_buffer[8] as u32) << 24
+                        | (xing_buffer[9] as u32) << 16
+                        | (xing_buffer[10] as u32) << 8
+                        | xing_buffer[11] as u32;
                     let rate = sampling_rate as u64;
                     let billion = 1000000000;
                     let frames_x_samples = num_frames as u64 * num_samples as u64;
@@ -223,9 +260,11 @@ pub fn from_read<T>(reader: &mut T) -> Result<Duration, Error>
             let bitrate = get_bitrate(version, layer, encoded_bitrate as u8)?;
             let frame_length = (num_samples / 8 * bitrate / sampling_rate + padding) as usize;
 
-            match skip(reader,
-                       &mut dump,
-                       frame_length - header_buffer.len() - xing_offset - xing_buffer.len()) {
+            match skip(
+                reader,
+                &mut dump,
+                frame_length - header_buffer.len() - xing_offset - xing_buffer.len(),
+            ) {
                 Ok(_) => (),
                 Err(ref e) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
                 Err(e) => bail!(e),
@@ -238,8 +277,9 @@ pub fn from_read<T>(reader: &mut T) -> Result<Duration, Error>
         }
 
         // ID3v2 frame
-        let is_id3v2 = header_buffer[0] == 'I' as u8 && header_buffer[1] == 'D' as u8 &&
-                       header_buffer[2] == '3' as u8;
+        let is_id3v2 = header_buffer[0] == 'I' as u8
+            && header_buffer[1] == 'D' as u8
+            && header_buffer[2] == '3' as u8;
         if is_id3v2 {
             let mut id3v2 = [0; 6]; // 4 bytes already read
             match reader.read_exact(&mut id3v2) {
@@ -249,9 +289,10 @@ pub fn from_read<T>(reader: &mut T) -> Result<Duration, Error>
             };
             let flags = id3v2[1];
             let footer_size: usize = if 0 != (flags & 0b00010000) { 10 } else { 0 };
-            let tag_size: usize =
-                ((id3v2[5] as u32) | ((id3v2[4] as u32) << 7) | ((id3v2[3] as u32) << 14) |
-                 ((id3v2[2] as u32) << 21)) as usize;
+            let tag_size: usize = ((id3v2[5] as u32)
+                | ((id3v2[4] as u32) << 7)
+                | ((id3v2[3] as u32) << 14)
+                | ((id3v2[2] as u32) << 21)) as usize;
             match skip(reader, &mut dump, tag_size + footer_size) {
                 Ok(_) => (),
                 Err(ref e) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
@@ -261,8 +302,9 @@ pub fn from_read<T>(reader: &mut T) -> Result<Duration, Error>
         }
 
         // ID3v1 frame
-        let is_id3v1 = header_buffer[0] == 'T' as u8 && header_buffer[1] == 'A' as u8 &&
-                       header_buffer[2] == 'G' as u8;
+        let is_id3v1 = header_buffer[0] == 'T' as u8
+            && header_buffer[1] == 'A' as u8
+            && header_buffer[2] == 'G' as u8;
         if is_id3v1 {
             match skip(reader, &mut dump, 128 - header_buffer.len()) {
                 Ok(_) => (),
@@ -310,7 +352,8 @@ pub fn from_file(file: &File) -> Result<Duration, Error> {
 /// println!("File duration: {:?}", duration);
 /// ```
 pub fn from_path<P>(path: P) -> Result<Duration, Error>
-    where P: AsRef<Path>
+where
+    P: AsRef<Path>,
 {
     let file = File::open(path)?;
     from_file(&file)
